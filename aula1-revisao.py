edb31984266f79cd3c1a44962e765acf117609fc
NicolasOliveira01 <128194431+NicolasOliveira01@users.noreply.github.com>
2024-08-30 17:53:54 -0300

    aula1-revisao.py

--- aula1-revisao.py
+++ aula1-revisao.py
@@ -1,4 +1,4 @@
-'''exercicio 1
+exercicio 1
 
 num1 = int(input("Escreva um numero: "))
 num2 = int(input("Escreva outro numero: "))
@@ -7,9 +7,9 @@ if num1 > num2:
     print(f"O maior numero e o {num1}")
 else:
     print(f"O maior numero e o {num2}")
-'''
 
-'''exercicio 2
+
+exercicio 2
 
 nasc = int(input("Digite o ano que voce nasceu: "))
 idade = 2023 - nasc
@@ -18,9 +18,9 @@ if idade < 16:
     print("Voce nao podera votar esse ano")
 else:
     print("Voce podera votar esse ano")
-'''
 
-'''exercicio 3 
+
+exercicio 3 
 
 senha = int(input("Digite sua senha: "))
 
@@ -28,9 +28,9 @@ if senha == 1234:
     print("ACESSO PERMITIDO")
 else:
     print("ACESSO NEGADO")
-'''
 
-'''exercicio 4
+
+exercicio 4
 
 quantidade = int(input("Digite o numero de macas que voce comprou: "))
 preco = quantidade * 0.30
@@ -40,9 +40,9 @@ if quantidade < 6:
 else:
     preco = quantidade * 0.25
     print(f"O preco pago e de {preco}: ")
-'''
 
-'''exercicio 5
+
+exercicio 5
 
 num1 = float(input("Digite um numero: "))
 num2 = float(input("Outro um numero: "))
@@ -54,9 +54,8 @@ elif num2 > num3:
     print(f"O maior numero e o {num2}")
 else:
     print(f"O maior numero e o {num3}")
-'''
 
-'''exercicio 6
+exercicio 6
 
 sexo = int(input("Digite 1 para feminino e 2 para masculino: "))
 altura = float(input("Digite sua altura: "))
@@ -67,9 +66,9 @@ if sexo == 1:
 elif sexo == 2:
     peso2 = (72.7 * altura) - 58
     print(f"O seu peso ideal {peso2}")
-'''
 
-'''exercicio 7 e 8
+
+exercicio 7 e 8
 
 lados = int(input("Digite o numero de lados do poligono: "))
 
@@ -87,9 +86,9 @@ elif lados == 5:
     print("Esse poligono e um PENTAGONO")
 else:
     print("POLIGONO nao identificado")
-'''
 
-'''exercicio 9 
+
+exercicio 9 
 
 num1 = float(input("Digite um numero: "))
 num2 = float(input("Digite outro numero: "))
@@ -101,9 +100,9 @@ if num2 > maior:
 if num3 > maior:
     maior = num3
 print(f"O maior numero e o {maior}")
-'''
 
-'''exercicio 10
+
+exercicio 10
 
 lado1 = float(input("Digite o valor de um lado do triangulo: "))
 lado2 = float(input("Digite o valor de outro lado do triangulo: "))
@@ -115,9 +114,9 @@ elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
     print("Esse triangulo e isoscele")
 else:
     print("Esse triangulo e escaleno")
-'''
 
-'''exercicio 11
+
+exercicio 11
 
 angulo1 = float(input("Digite o valor de um angulo do triangulo: "))
 angulo2 = float(input("Digite o valor de outro angulo do triangulo: "))
@@ -129,8 +128,7 @@ elif angulo1 > 90 or angulo2 > 90 or angulo3 > 90:
     print("Triangulo Obtusangulo")
 elif angulo1 < 90 or angulo2 < 90 or angulo3 < 90:
     print("Triangulo Acutangulo")
-'''
-'''
+
 escolha = input("Digite se o primeiro jogador escolheu par ou impar: ")
 num1 = int(input("Digite o numero que o primeiro jogador escolheu: "))
 num2 = int(input("Digite o numero que o segundo jogador escolheu: "))
@@ -144,5 +142,4 @@ elif escolha == "par" and soma % 2 == 1:
     print("A soma deu um numero impar e o jogador 2 escolheu impar, portanto ele ganhou")
 else:
     print("A soma deu um numero par e o jogador 2 escolheu par, portanto ele ganhou")
-'''
 
